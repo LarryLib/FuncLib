4b3122eb15f818c0b0e4c29d548e2ef56234d3aa
lxd-Super-main <lxd_super_main@126.com>
2020-07-09 16:53:30 +0800

    first commit

---
+++ FuncLib/Classes/Func.h
@@ -0,0 +1,17 @@
+//
+//  Func.h
+//  FuncLib
+//
+//  Created by Larry Mac Pro on 2020/7/9.
+//  Copyright © 2020 lxd-Super-main. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Func : NSObject
+- (void)testPrint:(NSString *)text;
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ FuncLib/Classes/Func.m
@@ -0,0 +1,17 @@
+//
+//  Func.m
+//  FuncLib
+//
+//  Created by Larry Mac Pro on 2020/7/9.
+//  Copyright © 2020 lxd-Super-main. All rights reserved.
+//
+
+#import "Func.h"
+
+@implementation Func
+- (void)testPrint:(NSString *)text {
+    
+    NSLog(@"JCNetworkManager print == %@",text);
+    
+}
+@end
